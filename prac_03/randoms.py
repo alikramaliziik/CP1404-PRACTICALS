7d4d4fd16921f86a148ee6b5a29b699fc7cec90a
Akram kasozi <akram.kasozi@my.jcu.edu.au>
2025-05-27 15:31:40 +0300

    Print numbers within a range randomly

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -4,7 +4,7 @@ print(random.randint(5, 20))  # line 1
 print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
 
-# Answers to random function questions:
+# Answers to random function questions: 
 # Line 1: random.randint(5, 20)
 #   - Smallest number: 9
 #   - Largest number: 12
